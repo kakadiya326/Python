564b6bfd71488fad28b05931d5b443885214c0f0
kakadiya326 <kakadiyachiranj044@gmail.com>
2025-08-26 10:09:08 +0530

    Numeric Rectangle simple pattern

---
+++ numericRectangle.py
@@ -0,0 +1,13 @@
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(i if i<j else j,end=" ")
+    for j in range(n-1,0,-1):
+        print(i if i<j else j,end=" ")
+    print()
+for i in range(n-1,0,-1):
+    for j in range(1,n+1):
+        print(i if i<j else j,end=" ")
+    for j in range(n-1,0,-1):
+        print(i if i<j else j,end=" ")
+    print()
