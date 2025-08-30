b14931562be178844886374aa84e274f42b05c0c
kakadiya326 <kakadiyachiranj044@gmail.com>
2025-08-30 11:01:28 +0530

    display prime factors for given number n using nested for loops

---
+++ primeFactorsNFor.py
@@ -0,0 +1,9 @@
+#display prime factors for given number n.
+n=int(input("Enter a number: "))
+for i in range(2,n+1):
+    if n%i==0:
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            print(i)
