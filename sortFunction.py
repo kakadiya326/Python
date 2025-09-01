d89ecd9d55161b35c875d239f24854dcffc74806
kakadiya326 <kakadiyachiranj044@gmail.com>
2025-09-01 12:18:01 +0530

    sort and sorted function with reverse attribute

---
+++ sortFunction.py
@@ -0,0 +1,9 @@
+#sort and sorted function with reverse attribute
+l=[4,9,6,5,2,9]
+l.sort()
+print("sort function : ",l)
+l.sort(reverse=True)
+print("sort function with reverse : ",l)
+print("sorted function : ",sorted(l))
+print("sorted function with reverse : ",sorted(l,reverse=True))
+print(l)
