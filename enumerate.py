89d62670dcfa5d22be8520418863ced71139b6ce
kakadiya326 <kakadiyachiranj044@gmail.com>
2025-09-17 15:24:25 +0530

    To attach index to the list data enumerates() will use

---
+++ enumerate.py
@@ -0,0 +1,4 @@
+#To attach index to the list data enumerates() will use
+k=['a','b','c','d','e']
+print(list(enumerate(k))) #it will return list tuple with numeric index
+print(dict(enumerate(k))) #it will return dictionary of numeric index
