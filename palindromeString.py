da20b22e63f98f84c38338f6781aaa26aa6c44d1
kakadiya326 <kakadiyachiranj044@gmail.com>
2025-09-17 15:31:10 +0530

    Check given string is palindrome or not.

---
+++ palindromeString.py
@@ -0,0 +1,12 @@
+#Check given string is palindrome or not.
+s=input()
+i=0
+j=len(s)-1
+while i<j:
+    if s[i]!=s[j]:
+        print("Not a palindrome")
+        break
+    i+=1
+    j-=1
+else:
+    print("Palindrome")
