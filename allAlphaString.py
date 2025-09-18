9571f00b5ad427c6b7b22cb9a16d28428749b818
kakadiya326 <kakadiyachiranj044@gmail.com>
2025-09-18 15:13:41 +0530

    Solution-2

--- allAlphaString.py
+++ allAlphaString.py
@@ -1,10 +1,20 @@
 #Check given string contains all alphabets or not.
 
+#Solution-1
 s='abcdefghijklmnopqrstuvwxyz'
 
 for i in range(97,123):
     if chr(i) not in s:
         print(chr(i),' is not string')
+        break
+else:
+    print('All alphabets are present in string')
+
+#Solution -2
+s=input()
+for ch in 'abcdefghijklmnopqrstuvwxyz':
+    if ch not in s:
+        print(ch,' is not string')
         break
 else:
     print('All alphabets are present in string')
