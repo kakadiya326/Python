57a6a68ad8d2a6868f17c90d259044f2c859d770
kakadiya326 <kakadiyachiranj044@gmail.com>
2025-09-18 15:23:48 +0530

    Importing predefine code of string

---
+++ stringImport.py
@@ -0,0 +1,10 @@
+#Import string module
+import string
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
+print(string.ascii_letters)
+print(string.digits)
+print(string.hexdigits)
+print(string.whitespace)
+print(string.octdigits)
+print(string.printable)
