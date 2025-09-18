ebf95eaa93fa6893bf4d6ba93c1dc5040c80235d
kakadiya326 <kakadiyachiranj044@gmail.com>
2025-09-18 15:19:17 +0530

    Full Code

--- secondMaxF.py
+++ secondMaxF.py
@@ -1,11 +1,11 @@
 #Find second largest element from a given list
 #solution-1
-s1=[3,6,7,36,7,3,67,8,4,34,65]
-s1=sorted(set(s1),reverse=True)
-print(s1[1])
+# s1=[3,6,7,36,7,3,67,8,4,34,65]
+# s1=sorted(set(s1),reverse=True)
+# print(s1[1])
 
 #solution-2
-s2=[23,6,34,75,23,75,3,6,3,7]
+s2=[4,1,2,3]
 max1=max2=s2[0]
 for el in s2:
     if el>max1:
