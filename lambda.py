267324ba024c2c8650470e58263b7efc5bdb649f
kakadiya326 <kakadiyachiranj044@gmail.com>
2025-10-15 15:53:09 +0530

    to define single-line function or anonymous

---
+++ lambda.py
@@ -0,0 +1,11 @@
+#to define single-line function or anonymous
+print((lambda x:x+5)(6))
+
+add=lambda x,y:x+y
+print(add(10,5))
+
+isEven=lambda x:x%2==0
+print(isEven(6))
+
+sqr=lambda x:x*x
+print(sqr(3))
