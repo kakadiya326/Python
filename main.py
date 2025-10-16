90f4d0dcc80eb4ff33cef672ce6cc13df8b12ffa
kakadiya326 <kakadiyachiranj044@gmail.com>
2025-10-16 15:17:26 +0530

    How to make module

---
+++ main.py
@@ -0,0 +1,15 @@
+# import module
+# print(module.add(4,9))
+# print(module.PI)
+
+# import module as m
+# print(m.mul(3,12))
+# print(m.div(6,2))
+# print(m.sub(6,9))
+
+# from module import *
+# print(add(12,6))
+# print(fact(12))
+
+from module import mul
+print(mul(6,2))
---
+++ module.py
@@ -0,0 +1,16 @@
+#Module file.
+PI=3.14
+def fact(n):
+    if n==1:
+        return 1
+    else:
+        return n*fact(n-1)
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
