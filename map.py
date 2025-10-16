60a22f1e7a3b9b7cb39b6ef7da3c986f2bccf047
kakadiya326 <kakadiyachiranj044@gmail.com>
2025-10-16 15:18:01 +0530

    use of map()

---
+++ map.py
@@ -0,0 +1,8 @@
+#To apply given function to every element in sequence map will use.
+#Square every element in a given list.
+l=[4,5,2,9,3,0,1]
+print(list(map(lambda x:x*x,l)))
+
+import math
+l=[4,8,2,4,12,6,7,8]
+print(list(map(math.factorial,l)))
